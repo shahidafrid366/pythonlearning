8a8ab7688ac602035e66eb942fca3fec234ccaea
Shahid Afrid <55510234+shahidafrid366@users.noreply.github.com>
2025-10-19 23:51:13 +0530

    Update 1.1 python_intro.py

--- 1.1 python_intro.py
+++ 1.1 python_intro.py
@@ -1,11 +1,11 @@
-<h1>What is Python?</h1>
+What is Python?
 Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
 
 It is used for:
-*web development (server-side),
-*software development,
-*mathematics,
-*system scripting.
+    web development (server-side),
+    software development,
+    mathematics,
+    system scripting.
 
 
 What can Python do?
